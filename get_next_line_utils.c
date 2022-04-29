77adfd5ce67e001b7f049ab23538120188020cf6
Courinha768 <90760852+Courinha768@users.noreply.github.com>
2022-04-29 14:45:48 +0100

    Add files via upload

---
+++ get_next_line_utils.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aappleto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/27 22:26:50 by aappleto          #+#    #+#             */
+/*   Updated: 2022/04/27 22:26:51 by aappleto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (s[i])
+	{
+		if (s[i] == (char) c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*dup;
+	int		i;
+
+	i = 0;
+	while (s1[i])
+		i++;
+	dup = (char *)malloc(sizeof(char) * (i + 1));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (i < (int)ft_strlen(s1))
+	{
+		dup[i] = s1[i];
+		i++;
+	}
+	dup[i] = 0;
+	return (dup);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	if (!s1)
+		s1 = ft_strdup("");
+	if (!s1 || !s2)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+		i++;
+	j = 0;
+	while (s2[j])
+		j++;
+	str = malloc(sizeof(char) * (i + j + 1));
+	if (!str)
+		return (NULL);
+	j = 0;
+	ft_strlcpy(str, s1, i + 1);
+	while (s2[j])
+		str[i++] = s2[j++];
+	str[i] = 0;
+	free(s1);
+	return (str);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+
+	i = 0;
+	if (dstsize == 0)
+		return (ft_strlen(src));
+	while (i < dstsize - 1 && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = 0;
+	while (src[i])
+		i++;
+	return (i);
+}
